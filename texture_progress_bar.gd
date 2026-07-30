906a342cfff66cc421b09b6233cc27d3fd632dba
Joshn't <86410587+Joshnt@users.noreply.github.com>
2026-07-30 09:19:04 +0200

    Healthbar!

---
+++ texture_progress_bar.gd
@@ -0,0 +1,12 @@
+extends TextureProgressBar
+
+func set_progress(p: float) -> void:
+	value = clamp(p, min_value, max_value)
+
+func hurt() -> void:
+	if value == 0:
+		return
+	value = clamp(value - (max_value * 0.2), min_value, max_value)
+
+func _ready() -> void:
+	set_progress(100)
